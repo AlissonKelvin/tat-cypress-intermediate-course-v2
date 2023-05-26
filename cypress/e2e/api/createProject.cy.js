98f8ba187374c0bf150f2f341332a18f6e1a795f
alissonKelvin <alisson.kelvindecarvalho@hotmail.com>
2023-05-26 09:18:05 -0300

    create project via API

---
+++ cypress/e2e/api/createProject.cy.js
@@ -0,0 +1,29 @@
+
+import {faker} from '@faker-js/faker'
+
+describe('Create Project (API)',() =>{
+
+it('successfully create',() =>{
+
+    const project = {
+
+        name: `Project - ${faker.datatype.uuid()}`,
+        description: faker.random.words(5),
+
+    }
+
+
+    cy.api_createProject(project).then((response) =>{
+
+        const {status, body} = response
+
+        expect(status).to.equal(201)
+        expect(body.name).to.equal(project.name)
+        expect(body.description).to.equal(project.description)
+
+    })
+
+})
+
+
+})
